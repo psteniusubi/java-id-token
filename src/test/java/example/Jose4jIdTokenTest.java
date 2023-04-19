6e3e0add471b4cf92c297e383ca4e34f9b710f08
Petteri Stenius <petteri.stenius@ubisecure.com>
2023-04-19 11:24:41 +0300

    add example with jose4j library

--- src/test/java/example/Jose4jIdTokenTest.java
+++ src/test/java/example/Jose4jIdTokenTest.java
@@ -3,41 +3,26 @@ package example;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import java.net.URL;
-import java.time.Instant;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
-
+import org.jose4j.jwk.HttpsJwks;
+import org.jose4j.jwk.JsonWebKeySet;
+import org.jose4j.jwt.JwtClaims;
+import org.jose4j.jwt.NumericDate;
+import org.jose4j.jwt.consumer.JwtConsumer;
+import org.jose4j.jwt.consumer.JwtConsumerBuilder;
+import org.jose4j.keys.resolvers.DecryptionKeyResolver;
+import org.jose4j.keys.resolvers.HttpsJwksVerificationKeyResolver;
+import org.jose4j.keys.resolvers.JwksDecryptionKeyResolver;
+import org.jose4j.keys.resolvers.VerificationKeyResolver;
 import org.junit.jupiter.api.Test;
-
-import com.nimbusds.jose.JWSAlgorithm;
-import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
-import com.nimbusds.jose.jwk.source.JWKSource;
-import com.nimbusds.jose.jwk.source.JWKSourceBuilder;
-import com.nimbusds.jose.proc.DefaultJOSEObjectTypeVerifier;
-import com.nimbusds.jose.proc.JWEDecryptionKeySelector;
-import com.nimbusds.jose.proc.JWSKeySelector;
-import com.nimbusds.jose.proc.JWSVerificationKeySelector;
-import com.nimbusds.jose.proc.SecurityContext;
-import com.nimbusds.jwt.EncryptedJWT;
-import com.nimbusds.jwt.JWT;
-import com.nimbusds.jwt.JWTClaimsSet;
-import com.nimbusds.jwt.JWTParser;
-import com.nimbusds.jwt.PlainJWT;
-import com.nimbusds.jwt.proc.ConfigurableJWTProcessor;
-import com.nimbusds.jwt.proc.DefaultJWTClaimsVerifier;
-import com.nimbusds.jwt.proc.DefaultJWTProcessor;
-import com.nimbusds.jwt.proc.JWTClaimsSetVerifier;
 
 /**
  * Encrypted id_token was captured with
  * https://github.com/psteniusubi/python-sample
+ * 
+ * This example uses JWT library from
+ * https://bitbucket.org/b_c/jose4j/wiki/Home
  */
-public class IdTokenTest {
+public class Jose4jIdTokenTest {
     // this id_token was captured with code-flow-with-jwsreq.py
     static String ID_TOKEN = "eyJjdHkiOiJKV1QiLCJhbGciOiJSU0EtT0FFUCIsImtpZCI6Ijc3OTllNzBhLTUzYzEtNDZjNi1iNGI1LTM5NzkwZGVlYTk5NyIsImVuYyI6IkExMjhDQkMtSFMyNTYifQ.FuVhmIdj2mOf9UPFPLbSeHKQIAUDvNVQxDV3X2MhE6MEiucD1tOLjRDMIuSPIVK9Co518OCvy-PskuMhxCPdmcCnbqI7YXa_ZZ2L2DATMM_nJNqpELuhn8SMEhXgmq9JHRQOu_FtejCr1T1pXL9-Jh8Qb49Ka9DnivqLqh388aZgmoIGOWg_kqF__qZgIRBsrw2L29aZX8GnzRkniUMXjy2-BkbpfdOWgEidvgZgDuBOZ5afaK32NOrkoTiSp_l2fbdo22R4A3v58T00es6ddTj1b1RAqi2nEmYwXRvw5OIsSFOToy-wdIaEetnSYigMPU675ZrRse-E4A8chZxHBQ.oqNpnfCf7swEHvK0c-7uYw.ScCiyQ8EsgOqblu3okL-_NC4Z9GBOIU3-ek97r264UETqp0lCgSswiTiQLZE0VAdd1WOZnXbAnvMmUGhdqhT5MFFSGNj0xCotOk-756EnJF2Dj2imNuSj4fC6CxnLAdjWXwv9zRUaWa30RkXai-zPEvLwGGuAUvS2waY5fP1ME0M60nnaYBxplNeRpATQDDBxt8kK_cI1t1yUh8PLk1iutIAx57niyvZutgzvh5m5FKGMEEfPndXNJ2s9FBYjWhlqU7-GEplvjoLo1o8Ial9Y9UXggp8j_0l_kdwvu1LG-F58vnuPWpE3UC03exonL0J3Z2NSsazEhxE2h5zZRl1Cy-680Msr9jCOyiMBrS68HB6GyOv1ThiDQjJaPfCA9IQj4_qTopBCmWH_rrXVu2IWD2CmbUKSZk6A1-djt7o-g8QdQVOG1omueTWchPqLjdyUJyNxbNxtu5V-JVXTY3T87uBXnnTeXb3hRbhehDqnZE84rvjwDqfyWnLZcGQmUtD6U3rkv7hEFdgr_3IMEQgknzO6FXl4VmXtXJJ39XNPvjfdBelt4b50xOG2leHCpWdhRsfaVRG0uoOX2taCGzRUKo93e3zq6qtdXOWPj6ciAlvS54OPTFOLeys-PMqPUO6jw8-_z23i4kpScAMvRNe09MyomkhRvQsdFv2KtRxwL1nLMp5KnqjuXKlDnHdgx8hsgild3vUSFAvTea_I_R2o4jvYyHGI-48uanFkyPI6hSmDbDIcR6pL5vAFNszIJ1RBMB2sDkWQN9yefH_g7ptPsTNLthgzixhxKLlI_v13WFIbICPJ1CtNecqi8KgF6_B5E1AzblLwe-a0q5Qn-wZRIyXNOYM58ccBrsbD-41c2y_WnXWjiP-E4aA0px3hDaBAyGqYoEX8fmKaheUUbrKsLOSU0H4Snsv5Acn2WsD_ELI8WfH_PKfj8ZNS425Zl3m3hnccHfkEg3WZp7LOttYmjazvFD6_GdzS52HLhP06k8w5ANfff044KhTQuztOJI-FND-p_coiPlzTXhA6QY4sK2pvtGa7SHEf4PUQv0v5ogv1FoIRRVz7J781pM7xz5V557R7bjb15us2HB84WROt73dxxvEBebW5-gIMtgbWORCnqZao7xRqRPcIq9mfaCkR95OKw2USEcWeWCOXvPrMTpBSy5pVmXvsm1gXm8Fb7ba_4f-eFt1zdmxdZwpnVOUrph-0G9EFsxivdHRFwZA3-eQgZ0m3NjdKufZRT1QmoSK_XNYF5SYcKBQZuaEI6l5XzeP7-PjMBAylWyZv_20jbJxSIFkM8j6PaOXp5NxSdwnGKq132AHq2hxXMRrEcbXWTedHxBNWHJmDdX_-UYthgnB32nzjPq-4s_j7PEDjkYUznHqBPTQwtJy4Rk7btchoAZuzSP8YWICNcEUUnorVC25H6S4RL-a2veRNNdMbn7SKTidUwxMtcsFfJRS_0LLrB5F1KHKHdDFFMIesSwGh8pZ9uycU1oNE4fD28O_wfJDvl-4QBYg3LHFvDS58vSiKGQQqf-PnHtJ8FhLzrJp8A7szZflqOGjltEXtw__gZ4.BhKXHyZqJW9pXMBqCfi8Yg";
     // this jwk is from code-flow-with-jwsreq.jwk
@@ -47,79 +32,38 @@ public class IdTokenTest {
 
     @Test
     public void test1() throws Exception {
-
-        ConfigurableJWTProcessor<SecurityContext> proc = new DefaultJWTProcessor<>();
-
-        // replace with id_token from OIDC token response 
-        JWT token = JWTParser.parse(ID_TOKEN);
-
-        if (token instanceof PlainJWT) {
-            // plain not encrypted and/or unsigned token is not accepted
-            throw new IllegalArgumentException();
-        }
-        if (token instanceof EncryptedJWT) {
-            // if id token is encrypted then create JWEDecryptionKeySelector
-            // replace JWKS with your application's keys
-            JWKSet jwks = JWKSet.parse(JWKS);
-            EncryptedJWT jwe = (EncryptedJWT) token;
-            // make sure content type (cty) is JWT
-            if(!"JWT".equals(jwe.getHeader().getContentType())) {
-                throw new IllegalArgumentException();
-            }
-            JWEDecryptionKeySelector<SecurityContext> jweKeySelector = new JWEDecryptionKeySelector<>(
-                    jwe.getHeader().getAlgorithm(), jwe.getHeader().getEncryptionMethod(), new ImmutableJWKSet<>(jwks));
-            proc.setJWEKeySelector(jweKeySelector);
-            // make sure token type (typ) is JWT
-            proc.setJWETypeVerifier(DefaultJOSEObjectTypeVerifier.JWT);
+        
+        // use current time when null
+        NumericDate evaluationTime = null;
+        if(TESTING) {
+            // for testing, use fixed time
+            evaluationTime = NumericDate.fromSeconds(1681402772);
         }
 
-        // make sure token type (typ) is JWT
-        proc.setJWSTypeVerifier(DefaultJOSEObjectTypeVerifier.JWT);
-
-        // accept any RSA signature format
-        Set<JWSAlgorithm> jwsAlgs = new HashSet<>(
-                Arrays.asList(JWSAlgorithm.RS256, JWSAlgorithm.RS384, JWSAlgorithm.RS512));
-
-        // fetch jwks from ID provider
-        JWKSource<SecurityContext> jwsJwkSource = JWKSourceBuilder
-                .create(new URL("https://login.example.ubidemo.com/uas/oauth2/metadata.jwks")).build();
-        // create JWSVerificationKeySelector
-        JWSKeySelector<SecurityContext> jwsKeySelector = new JWSVerificationKeySelector<>(jwsAlgs, jwsJwkSource);
-        proc.setJWSKeySelector(jwsKeySelector);
-
-        // allowed audience is client_id
-        Set<String> audience = new HashSet<>(Arrays.asList("2af1c7c9-04dc-40d0-abac-103cee2a821d"));
-        // validate iss claim
-        JWTClaimsSet expected = new JWTClaimsSet.Builder().issuer("https://login.example.ubidemo.com/uas").build();
-        if (TESTING) {
-            // for testing, validate iss, iat and exp claims
-            expected = new JWTClaimsSet.Builder().issuer("https://login.example.ubidemo.com/uas")
-                    .issueTime(Date.from(Instant.ofEpochSecond(1681402772)))
-                    .expirationTime(Date.from(Instant.ofEpochSecond(1681406371))).build();
-        }
-        // require sub, iat and exp claims be present
-        Set<String> required = new HashSet<>(Arrays.asList("sub", "iat", "exp"));
-        // no prohibited claims
-        Set<String> prohibited = Collections.emptySet();
-
-        // create DefaultJWTClaimsVerifier
-        JWTClaimsSetVerifier<SecurityContext> verifier = new DefaultJWTClaimsVerifier<>(audience, expected, required,
-                prohibited);
-        if (TESTING) {
-            // for testing, override currentTime
-            verifier = new DefaultJWTClaimsVerifier<>(audience, expected, required, prohibited) {
-                @Override
-                protected Date currentTime() {
-                    return Date.from(Instant.ofEpochSecond(1681402772));
-                }
-            };
-        }
-        proc.setJWTClaimsSetVerifier(verifier);
+        // create jwe decryption key resolver
+        JsonWebKeySet jwks = new JsonWebKeySet(JWKS);
+        DecryptionKeyResolver jwe = new JwksDecryptionKeyResolver(jwks.getJsonWebKeys());
+        
+        // create jws validation key resolver
+        HttpsJwks httpsJwks = new HttpsJwks("https://login.example.ubidemo.com/uas/oauth2/metadata.jwks");
+        VerificationKeyResolver jws = new HttpsJwksVerificationKeyResolver(httpsJwks);
+        
+        // create jwt claims validator
+        JwtConsumer jwtConsumer = new JwtConsumerBuilder()
+                .setRequireExpirationTime()
+                .setRequireIssuedAt()
+                .setRequireSubject()
+                .setExpectedIssuer(true, "https://login.example.ubidemo.com/uas")
+                .setExpectedAudience(true, "2af1c7c9-04dc-40d0-abac-103cee2a821d")
+                .setEvaluationTime(evaluationTime)
+                .setDecryptionKeyResolver(jwe)
+                .setVerificationKeyResolver(jws)
+                .build();
 
         // validate id token and get claims
-        JWTClaimsSet claims = proc.process(token, null);
+        JwtClaims claims = jwtConsumer.processToClaims(ID_TOKEN);
         assertNotNull(claims);
         assertEquals("https://login.example.ubidemo.com/uas", claims.getIssuer());
-
+        
     }
 }
